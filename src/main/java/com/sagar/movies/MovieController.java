15a63147841ad8658eb0c5310b93a26d9a4c1e54
Sagar Subedi <sagar@example.com>
2023-01-22 13:57:32 -0600

    feat: add controller

---
+++ src/main/java/com/sagar/movies/MovieController.java
@@ -0,0 +1,19 @@
+package com.sagar.movies;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllMovies(){
+        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    }
+}
