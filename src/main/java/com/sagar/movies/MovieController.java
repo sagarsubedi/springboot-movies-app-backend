bfc56834a6cff768df9d00939e3e2017543aa915
Sagar Subedi <sagar@example.com>
2023-01-22 17:13:44 -0600

    fix: add cors for frontend app

--- src/main/java/com/sagar/movies/MovieController.java
+++ src/main/java/com/sagar/movies/MovieController.java
@@ -4,16 +4,14 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.management.MonitorInfo;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://springbootmoviefrontend.sagarsubedi.com"})
 @RequestMapping("/api/v1/movies")
 public class MovieController {
 
